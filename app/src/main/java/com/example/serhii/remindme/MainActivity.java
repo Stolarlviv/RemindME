dd502c01a8a91a0773002d02e15e9e784c89cf05
Fit <Stolarlviv@gmail.com>
2016-12-30 12:21:44 +0200

    create Project

--- app/src/main/java/com/example/serhii/remindme/MainActivity.java
+++ app/src/main/java/com/example/serhii/remindme/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);hmhg
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
